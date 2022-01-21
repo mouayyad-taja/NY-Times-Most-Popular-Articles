fce9f7d0ebfd4625cef4e13c4201d0bd66a6df8e
Mouayyad Taja <mouayyad.taja@gmail.com>
2022-01-21 09:29:00 +0400

    UI simple tests

--- NY-Times-Most-Popular-ArticlesUITests/NY_Times_Most_Popular_ArticlesUITests.swift
+++ NY-Times-Most-Popular-ArticlesUITests/NY_Times_Most_Popular_ArticlesUITests.swift
@@ -39,4 +39,43 @@ class NY_Times_Most_Popular_ArticlesUITests: XCTestCase {
             }
         }
     }
+    
+    func testTableInteraction() {
+        let app = XCUIApplication()
+
+        // Assert that we are displaying the tableview
+        app.launch()
+
+        sleep(15)
+        let cells = app.tables.cells
+
+        // Get an array of cells
+        if cells.count > 0 {
+            let count: Int = (cells.count - 1)
+            
+            let promise = expectation(description: "Wait for table cells")
+            
+            for i in stride(from: 0, to: count , by: 1) {
+                // Grab the first cell and verify that it exists and tap it
+                let tableCell = cells.element(boundBy: i)
+                
+                XCTAssertTrue(tableCell.exists, "The \(i) cell is in place on the table")
+                
+                // Does this actually take us to the next screen
+                tableCell.tap()
+                
+                if i == (count - 1) {
+                    promise.fulfill()
+                }
+
+                // Back
+                app.navigationBars.buttons.element(boundBy: 0).tap()
+            }
+            
+            waitForExpectations(timeout: 50, handler: nil)
+            XCTAssertTrue(true, "Finished validating the table cells")
+        } else {
+            XCTAssert(false, "Was not able to find any table cells")
+        }
+    }
 }
